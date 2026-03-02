29e8a45840a3a8f60df04edd96e41908a3c0c6f1
RainButterfly <2771960681@qq.com>
2026-03-02 18:39:30 +0800

    Add files via upload

---
+++ GraphWeaverPlugin/Source/GraphWeaverPlugin/Private/GraphNode.cpp
@@ -0,0 +1,81 @@
+// Copyright 2026 RainButterfly. All Rights Reserved.
+
+#include "GraphNode.h"
+#include "FunctionTools.h"
+#include "GraphView.h"
+#include "Engine/Engine.h"
+
+UGraphNode::UGraphNode()
+{
+	SelfOuter = nullptr;
+	IndexInRealNodes = -1;
+	ExplicitName = "None";
+	SourceGraphView = nullptr;
+}
+
+UGraphNode::~UGraphNode()
+{
+}
+
+
+
+
+
+bool UGraphNode::ProcessInformAuto(UGraphView* InGraph)
+{
+	//每次当调用SpawnGraphNode并且勾选AutoBuild的时候必然会执行该函数(InGraph会由SpawnGraphNode::ExpandNode自行检查)
+	//执行该函数的时候IndexInRecorded必定是-1
+	if (InGraph)
+	{
+		InGraph->AddNewNodeIntelligent(this);
+	}
+	else
+	{
+		//SpawnGraphNode::ValidateNodeDuringCompilation
+		//实际上下面的报错大概率不会发生,主要由上面的函数和GetBlueprint()->Status = BS_Dirty来确保TargetView正确
+		UE_LOG(LogTemp, Error, TEXT("UGraphNode::ProcessInformAuto: InGraph == nullptr"));
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("UGraphNode::ProcessInformAuto: InGraph == nullptr"));
+	}
+	return true;
+}
+
+FGraphNodeDescription& UGraphNode::ObtainSelfDescription()
+{
+	if (IndexInRealNodes == -1)[[unlikely]]
+		return SourceGraphView->RealNodes[0];
+	return SourceGraphView->RealNodes[IndexInRealNodes];
+}
+
+TArray<int32> UGraphNode::ObtainAllActivatedChildDescription()
+{
+	return UFunctionTools_GraphWeaver::ObtainAllActivatedChildDescription(SourceGraphView, ObtainSelfDescription());
+}
+
+TArray<int32> UGraphNode::ObtainAllActivatedBroDescription()
+{
+	return UFunctionTools_GraphWeaver::ObtainAllActivatedBroDescription(SourceGraphView, ObtainSelfDescription());
+}
+
+TArray<int32> UGraphNode::ObtainDirectActivatedChildAndBroDescription()
+{
+	return UFunctionTools_GraphWeaver::ObtainDirectActivatedChildAndBroDes(SourceGraphView, ObtainSelfDescription());
+}
+
+TArray<int32> UGraphNode::ObtainAllChildDescription()
+{
+	return UFunctionTools_GraphWeaver::ObtainAllChildDes(SourceGraphView, ObtainSelfDescription());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
